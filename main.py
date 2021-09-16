c2004e916c383fc5626a85e90e06d7a8032db831
Youssef Mohamed <youssefmohamedshihata@gmail.com>
2021-09-16 10:47:25 +0200

    start play

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 def main():
     arr = ["-", "-", "-", "-", "-", "-", "-", "-", "-"]
 
-    print(arr[0] + " | " + arr[1] + " | " + arr[2])
-    print("---------")
-    print(arr[3] + " | " + arr[4] + " | " + arr[5])
-    print("---------")
-    print(arr[6] + " | " + arr[7] + " | " + arr[8])
+    i = 0
+
+    prev_step = "X"
+
+    while i < 9:
+        print(arr[0] + " | " + arr[1] + " | " + arr[2])
+        print("---------")
+        print(arr[3] + " | " + arr[4] + " | " + arr[5])
+        print("---------")
+        print(arr[6] + " | " + arr[7] + " | " + arr[8])
+
+        if prev_step == "X":
+            prev_step = "O"
+            position = int(input(f"Put the Position you want to put {prev_step} in [1, 9]: "))
+            arr[position - 1] = prev_step
+        elif prev_step == "O":
+            prev_step = "X"
+            position = int(input(f"Put the Position you want to put {prev_step} in [1, 9]: "))
+            arr[position - 1] = prev_step
+
+        i += 1
 
 main()
 
